44f83be8ea865753b14307b2742102aee9faf5fe
srinikesh2137 <srinikesh1728@gmail.com>
2026-09-19 13:49:36 +0530

    Add Week 4 practice problem: 2_BestTimeToBuySellStock.java

---
+++ practice problem/2_BestTimeToBuySellStock.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class BestTimeToBuySellStock {
+    static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        System.out.println("Maximum Profit: " + maxProfit(prices));
+        sc.close();
+    }
+}
