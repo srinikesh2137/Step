c7f5452618b9bf53aa30dc18b9bd517b8224a206
srinikesh2137 <srinikesh1728@gmail.com>
2026-09-19 13:48:03 +0530

    Add Week 3 assignment problem: 14_FirstValidMultiplicationTable.java

---
+++ assignment/14_FirstValidMultiplicationTable.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class FirstValidMultiplicationTable {
+    static void generateFirstValidTable(int[] candidates) {
+        for (int number : candidates) {
+            if (number < 1) {
+                System.out.println("Skipping invalid number: " + number);
+                continue;
+            }
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(number + " x " + i + " = " + (number * i));
+            }
+            break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of candidates: ");
+        int n = sc.nextInt();
+
+        int[] candidates = new int[n];
+        for (int i = 0; i < n; i++) {
+            candidates[i] = sc.nextInt();
+        }
+
+        generateFirstValidTable(candidates);
+        sc.close();
+    }
+}
