0628de47026c0f9826270bfb283629508e4c38fe
srinikesh2137 <srinikesh1728@gmail.com>
2026-09-19 13:44:31 +0530

    Add Week 3 practice problem: 2_RightAngledTrianglePattern.java

---
+++ practice problem/2_RightAngledTrianglePattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RightAngledTrianglePattern {
+    static void printTriangle(int rows) {
+        System.out.println("The right-angled triangle pattern for " + rows + " rows is");
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        printTriangle(rows);
+        sc.close();
+    }
+}
