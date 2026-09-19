eac217726c9bf694ff17251fbf616e784355069f
srinikesh2137 <srinikesh1728@gmail.com>
2026-09-19 13:47:27 +0530

    Add Week 3 assignment problem: 2_PositiveNegativeZeroClassifier.java

---
+++ assignment/2_PositiveNegativeZeroClassifier.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PositiveNegativeZeroClassifier {
+    static void classifyNumber(int number) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+        classifyNumber(number);
+        sc.close();
+    }
+}
