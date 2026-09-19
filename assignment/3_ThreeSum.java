082fec11b88c71011f38c03947d08a686f2096c8
srinikesh2137 <srinikesh1728@gmail.com>
2026-09-19 13:50:50 +0530

    Add Week 4 assignment problem: 3_ThreeSum.java

---
+++ assignment/3_ThreeSum.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class ThreeSum {
+    static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    int leftValue = nums[left];
+                    int rightValue = nums[right];
+
+                    while (left < right && nums[left] == leftValue) left++;
+                    while (left < right && nums[right] == rightValue) right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+
+        System.out.println("Unique triplets: " + threeSum(nums));
+        sc.close();
+    }
+}
