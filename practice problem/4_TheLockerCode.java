3b0ab9df67c95e0c51687d43ded80dc99a5e4264
srinikesh2137 <srinikesh1728@gmail.com>
2026-09-20 10:06:15 +0530

    Add Week 7 encapsulation practice problem

---
+++ practice problem/4_TheLockerCode.java
@@ -0,0 +1,27 @@
+public class TheLockerCode {
+    static class Locker {
+        private String combination;
+        private final int lockerNumber;
+
+        Locker(int lockerNumber, String combination) {
+            this.lockerNumber = lockerNumber;
+            this.combination = combination;
+        }
+
+        void changeCode(String currentCode, String newCode) {
+            if (combination.equals(currentCode)) {
+                combination = newCode;
+                System.out.println("Code changed successfully");
+            } else {
+                System.out.println("Code change rejected");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Locker l = new Locker(101, "1234");
+
+        l.changeCode("1234", "5678");
+        l.changeCode("0000", "9999");
+    }
+}
