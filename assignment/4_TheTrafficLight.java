20c9d61f3a6ed3d2d5504ab1f6367a0d6ad4fda8
srinikesh2137 <srinikesh1728@gmail.com>
2026-09-20 10:07:42 +0530

    Add Week 7 encapsulation assignment solution

---
+++ assignment/4_TheTrafficLight.java
@@ -0,0 +1,37 @@
+public class TheTrafficLight {
+    static class TrafficLight {
+        private String color;
+        private final String id;
+
+        TrafficLight(String id) {
+            this.id = id;
+            this.color = "RED";
+        }
+
+        void next() {
+            if (color.equals("RED")) {
+                color = "GREEN";
+            } else if (color.equals("GREEN")) {
+                color = "YELLOW";
+            } else {
+                color = "RED";
+            }
+        }
+
+        String getColor() {
+            return color;
+        }
+    }
+
+    public static void main(String[] args) {
+        TrafficLight t = new TrafficLight("TL-9");
+
+        System.out.println(t.getColor());
+        t.next();
+        System.out.println(t.getColor());
+        t.next();
+        System.out.println(t.getColor());
+        t.next();
+        System.out.println(t.getColor());
+    }
+}
